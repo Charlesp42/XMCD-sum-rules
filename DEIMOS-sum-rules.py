0713a142243a9b782cf9df93f6e81ec5a127e49c
Charlesp42 <82869857+Charlesp42@users.noreply.github.com>
2021-06-29 16:02:17 +0200

    Update DEIMOS-sum-rules.py

--- DEIMOS-sum-rules.py
+++ DEIMOS-sum-rules.py
@@ -4,7 +4,7 @@ FR
 Permet de normaliser, recentrer sur 0, combiner et les spectres XAS/XMCD issues
 de DEIMOS-SOLEIL puis d'appliquer automatiquement les règles de somme.
 Un numéro de scan en entrée (avec un paramètre Tz sans incidence sur les calculs),
-les 7 suivants étant le même échantillon avec +/-H et circulaire droite (CR) et gauche (CL).
+les 7 suivants étant le même échantillon avec +/-H et circulaire droit (CR) et gauche (CL).
 
 EN
 Allow the import, normalisation, centring and merging of XAS/XMCD spectra from
